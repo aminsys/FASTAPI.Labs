eb6b5bff72687b2552c74a25dffe22347fb201e3
Amin <amin.yassin@live.com>
2022-03-14 07:38:33 +0100

    User schema and response model

--- App/schemas.py
+++ App/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 # Defning what a post should look like. Getting validation for free.
 # This way we can guarantee that the front end will send the type of data that we want.
@@ -9,11 +9,23 @@ class PostBase(BaseModel):
     content: str
     published: bool = True
 
+class UserCreate(BaseModel):
+    email: EmailStr # Validate that this is valid email and not some random string
+    password: str
+
 
 class PostCreate(PostBase):
     pass
 
 class PostResponse(PostBase):
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
     created_at: datetime
 
     class Config:
